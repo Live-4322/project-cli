9d86d5b18fce78638e2325ae16ccbdd266325286
Charlie Oster <oster.charlie@yahoo.com>
2020-01-17 23:29:33 +0000

    modified location

--- spec/now-playing/movie_spec.rb
+++ spec/now-playing/movie_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe NowPlaying::Movie do
   context 'Class Methods' do
     # If you delete the fixtures or VCR generates new cassettes these tests
     # will have to be updated based on current values from:
-    # http://www.imdb.com/movies-in-theaters/
+    # http://imdb.com/movies-in-theaters
     around(:each) do |example|
       VCR.use_cassette("now-playing") do
         example.run
@@ -19,8 +19,8 @@ RSpec.describe NowPlaying::Movie do
       end
 
       it 'correctly scrapes names and urls for the movies' do
-        expect(movies.first.name).to eq("The 33 (2015)")
-        expect(movies.first.url).to eq("http://imdb.com/title/tt2006295/")
+        expect(movies.first.name).to eq("Bad Boys For Life (2020)")
+        expect(movies.first.url).to eq("http://imdb.com/movies-in-theaters")
       end
     end
 
@@ -32,30 +32,30 @@ RSpec.describe NowPlaying::Movie do
 
     describe '.find_by_name' do
       it 'returns the movie based on the name' do
-        expect(NowPlaying::Movie.find_by_name("The 33")).to eq(movies[0])
+        expect(NowPlaying::Movie.find_by_name("Bad Boys For Life")).to eq(movies[0])
       end
     end
   end
 
   context 'Instance Methods' do
-    subject{NowPlaying::Movie.new("The 33 (2015)", "http://imdb.com/title/tt2006295/")}
+    subject{NowPlaying::Movie.new("Bad Boys For Life (2020)", "https://www.imdb.com/title/tt1502397/?ref_=inth_ov_tt")}
 
     describe '#name' do
       it 'has a name' do
-        expect(subject.name).to eq("The 33 (2015)")
+        expect(subject.name).to eq("Bad Boys For Life (2020)")
       end
     end
 
     describe '#url' do
       it 'has a url' do
-        expect(subject.url).to eq("http://imdb.com/title/tt2006295/")
+        expect(subject.url).to eq("https://www.imdb.com/title/tt1502397/?ref_=inth_ov_tt")
       end
     end
 
     describe '#stars' do
       it 'has stars based on scraping the main URL' do
         VCR.use_cassette("movie") do
-          expect(subject.stars).to eq("Antonio Banderas, Rodrigo Santoro, Juliette Binoche")
+          expect(subject.stars).to eq(" Will Smith, Alexander Ludwig, Vanessa Hudgens, Michael Bay")
         end
       end
     end
@@ -63,7 +63,7 @@ RSpec.describe NowPlaying::Movie do
     describe '#summary' do
       it 'has a summary based on scraping the plotsummary URL' do
         VCR.use_cassette("plotsummary") do
-          expect(subject.summary).to eq("Based on the real-life event, when a gold and copper mine collapses, it traps 33 miners underground for 69 days.")
+          expect(subject.summary).to eq("The Bad Boys Mike Lowrey and Marcus Burnett are back together for one last ride in the highly anticipated Bad Boys for Life.")
         end
       end
     end
